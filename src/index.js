a8152757ac396f0532e65e2ac9615eb879076462
Branden Palmer <brandenpalmer08@gmail.com>
2022-01-05 11:31:04 +0700

    add class name query

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import makeDropDownMenu from "make-drop-down-menu";
 
 const mobileMenu = (() => {
-
-
+  const displayedItems = [
+    ...document.getElementsByClassName("item-displayed"),
+  ];
+  const hamburgeredItems = [
+    ...document.getElementsByClassName("item-under-icon"),
+  ];
 })();
