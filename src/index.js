6ac864353fde7b54f3d743e6211f357ba5620b80
Branden Palmer <brandenpalmer08@gmail.com>
2022-01-05 13:05:28 +0700

    add function to make hamburger button

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import makeDropDownMenu from "make-drop-down-menu";
+import "./style.scss";
 
 const mobileMenu = (() => {
-  const displayedItems = [
-    ...document.getElementsByClassName("item-displayed"),
-  ];
+  const displayedItems = [...document.getElementsByClassName("item-displayed")];
   const hamburgeredItems = [
     ...document.getElementsByClassName("item-under-icon"),
   ];
+
+  function _makeHamburgerButton() {
+    const button = document.createElement("button");
+    button.classList = "hamburger hamburger--spin";
+    button.type = "button";
+    button.addEventListener("click", () => {
+      button.classList.toggle("is-active");
+    });
+    const burgerBox = document.createElement("span");
+    burgerBox.classList = "hamburger-box";
+    const burgerInner = document.createElement("span");
+    burgerInner.classList = "hamburger-inner";
+    burgerBox.appendChild(burgerInner);
+    button.appendChild(burgerBox);
+    return button;
+  }
 })();
+export { mobileMenu };
