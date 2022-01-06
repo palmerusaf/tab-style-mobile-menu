90b2661d954b840fa1d1c89ef784f175a801453f
Branden Palmer <brandenpalmer08@gmail.com>
2022-01-06 13:16:04 +0700

    add make nav bar function

--- src/index.js
+++ src/index.js
@@ -2,10 +2,39 @@ import makeDropDownMenu from "make-drop-down-menu";
 import "./style.scss";
 
 const mobileMenu = (() => {
-  const displayedItems = [...document.getElementsByClassName("menu-item-displayed")];
-  const hamburgeredItems = [
+  const menuItemDisplayed = [
+    ...document.getElementsByClassName("menu-item-displayed"),
+  ];
+  const menuItemUnderIcon = [
     ...document.getElementsByClassName("menu-item-under-icon"),
   ];
+
+  document.body.appendChild(_makeNavBar());
+
+  function _makeNavBar() {
+    const nav = document.createElement("nav");
+    const listDisplayed = document.createElement("ul");
+    listDisplayed.classList = "list-displayed";
+    menuItemDisplayed.forEach((item) => {
+      const li = document.createElement("li");
+      li.classList = "list-displayed__item";
+      li.appendChild(item);
+      listDisplayed.appendChild(li);
+    });
+    nav.appendChild(listDisplayed);
+
+    const listUnderIcon = document.createElement("ul");
+    listUnderIcon.classList = "list-under-icon";
+    menuItemUnderIcon.forEach((item) => {
+      const li = document.createElement("li");
+      li.classList = "list-under-icon__item";
+      li.appendChild(item);
+      listDisplayed.appendChild(li);
+    });
+    nav.appendChild(listUnderIcon);
+
+    return nav;
+  }
 
   function _makeHamburgerButton() {
     const button = document.createElement("button");
