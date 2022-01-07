0e687e270b8614db83fc14f08ba638cb4fba00dd
Branden Palmer <brandenpalmer08@gmail.com>
2022-01-07 11:56:55 +0700

    change position of links

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const mobileMenu = (() => {
     nav.appendChild(listDisplayed);
 
     const listUnderIcon = _makeListUnderIcon();
-    document.body.appendChild(listUnderIcon)
+    document.body.prepend(listUnderIcon)
     const burgerIcon = _makeHamburgerButton(listUnderIcon);
     nav.appendChild(burgerIcon);
 
